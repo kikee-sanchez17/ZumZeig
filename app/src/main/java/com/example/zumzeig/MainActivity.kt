65bdb5468b75c259228353a817729075f0c06e54
Miriam Murà Calamardo <muracalamardo@gmail.com>
2024-05-28 01:35:24 +0200

    Bottom Nav Menu Done!!

--- app/src/main/java/com/example/zumzeig/MainActivity.kt
+++ app/src/main/java/com/example/zumzeig/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.example.zumzeig
-
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.MenuItem
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.Volley
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.zumzeig.R
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var fetchBtn: Button
-    private lateinit var logoutBtn: Button
-    private lateinit var emailEt: TextView
-    private lateinit var nameEt: TextView
-    private lateinit var userInfoEt: TextView
-    private lateinit var queue: RequestQueue
-    private lateinit var sharedPreferences: SharedPreferences
-    private var url: String="https://enricsanchezmontoya.cat/zumzeig/logout.php"
-
-    @SuppressLint("MissingInflatedId")
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var frameLayout: FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,50 +26,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        sharedPreferences = getSharedPreferences("UserInfo", MODE_PRIVATE)
-        emailEt = findViewById(R.id.emailEt)
-        nameEt = findViewById(R.id.nameEt)
-        userInfoEt = findViewById(R.id.userinfo)
-        queue = Volley.newRequestQueue(this)
-        fetchBtn = findViewById(R.id.fetch)
-        logoutBtn = findViewById(R.id.logout)
-        emailEt.setText(sharedPreferences.getString("email ",""))
-        nameEt.setText(sharedPreferences.getString("name",""))
-        Toast.makeText(this, sharedPreferences.getString("name",""), Toast.LENGTH_LONG).show()
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        frameLayout = findViewById(R.id.frameLayout)
 
-        logoutBtn.setOnClickListener{
-            Toast.makeText(this, "logged out", Toast.LENGTH_LONG).show()
-            val editor: SharedPreferences.Editor = sharedPreferences.edit()
-            editor.putString("logged","")
-            editor.putString("name","")
-            editor.putString("email","")
-            editor.putString("last_name", "")
-            editor.putString("country", "")
-            editor.putString("postal_code", "")
-            editor.putString("birthday", "")
-            editor.putString("phone_number", "")
-            editor.apply()
-            val intent= Intent(this, login::class.java)
-            startActivity(intent)
-            finish()
-        }
-        fetchBtn.setOnClickListener{
-            Toast.makeText(this, "fetch", Toast.LENGTH_LONG).show()
-// Obtenemos todos los campos almacenados en SharedPreferences
-            val allFields: Map<String, *> = sharedPreferences.all
-
-            // Creamos un StringBuilder para construir el string resultante
-            val stringBuilder = StringBuilder()
-
-            // Iteramos sobre todos los campos y los agregamos al StringBuilder
-            for ((key, value) in allFields) {
-                stringBuilder.append("$key: $value\n")
+        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            val itemId = item.itemId
+            when (itemId) {
+                R.id.navHome -> loadFragment(HomeFragment(), false)
+                R.id.navCalendar -> loadFragment(CalendarFragment(), false)
+                R.id.navSaved -> loadFragment(SavedFragment(), false)
+                else -> loadFragment(UserFragment(), false) // nav Profile
             }
-            userInfoEt.setText(stringBuilder)
-            userInfoEt.visibility= View.VISIBLE
 
+            true
+        }
+        loadFragment(HomeFragment(), true)
+    }
+
+    private fun loadFragment(fragment: Fragment, isAppInitialized: Boolean) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment)
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment)
         }
 
-
+        fragmentTransaction.commit()
     }
 }
